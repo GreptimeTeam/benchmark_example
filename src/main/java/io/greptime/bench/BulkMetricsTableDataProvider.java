c132a73df4b41d6b37ec4ac17734e94aa51e5159
luofucong <luofc@foxmail.com>
2025-06-10 19:10:22 +0800

    bulk insert metrics

---
+++ src/main/java/io/greptime/bench/BulkMetricsTableDataProvider.java
@@ -0,0 +1,158 @@
+package io.greptime.bench;
+
+import io.greptime.common.util.SystemPropertyUtil;
+import io.greptime.models.DataType;
+import io.greptime.models.TableSchema;
+
+import java.util.Iterator;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
+public class BulkMetricsTableDataProvider implements TableDataProvider {
+
+    private final TableSchema tableSchema;
+    private final long rowCount;
+    private final int serviceNumPerApp;
+
+    /*
+    ```sql
+    CREATE TABLE IF NOT EXISTS `tt_metrics_table` (
+        `ts` TIMESTAMP(3) NOT NULL,
+        `idc` STRING NULL INVERTED INDEX,
+        `host` STRING NULL INVERTED INDEX,
+        `shard` INT,
+        `service` STRING NULL INVERTED INDEX,
+        `url` STRING NULL,
+        `cpu_util` DOUBLE NULL,
+        `memory_util` DOUBLE NULL,
+        `disk_util` DOUBLE NULL,
+        `load_util` DOUBLE NULL,
+        TIME INDEX (`ts`),
+    )
+    PARTITION ON COLUMNS (shard) (
+        shard < 1,
+        shard >= 1
+    )
+    ENGINE=mito
+    WITH(
+        append_mode = 'true'
+    );
+    ```
+    */
+
+    {
+        this.tableSchema = TableSchema.newBuilder("tt_metrics_table")
+                .addTimestamp("ts", DataType.TimestampMillisecond)
+                .addTag("idc", DataType.String)
+                .addTag("host", DataType.String)
+                .addField("shard", DataType.Int32)
+                .addTag("service", DataType.String)
+                .addField("url", DataType.String)
+                .addField("cpu_util", DataType.Float64)
+                .addField("memory_util", DataType.Float64)
+                .addField("disk_util", DataType.Float64)
+                .addField("load_util", DataType.Float64)
+                .build();
+        this.rowCount = SystemPropertyUtil.getLong("table_row_count", 10_000_000_000L);
+        this.serviceNumPerApp = SystemPropertyUtil.getInt("tt_metrics_table.service_num_per_app", 20);
+    }
+
+    @Override
+    public void close() throws Exception {
+        // do nothing
+    }
+
+    @Override
+    public void init() {
+        // do nothing
+    }
+
+    @Override
+    public TableSchema tableSchema() {
+        return this.tableSchema;
+    }
+
+    @Override
+    public Iterator<Object[]> rows() {
+        ThreadLocalRandom random = ThreadLocalRandom.current();
+
+        return new Iterator<Object[]>() {
+
+            private long index = 0;
+
+            @Override
+            public boolean hasNext() {
+                return index < rowCount;
+            }
+
+            @Override
+            public Object[] next() {
+                index++;
+
+                long ts = System.currentTimeMillis();
+                String idc = nextIdc(random);
+                String host = nextHost(random, idc);
+                String app = nextApp(random, host);
+                String url = nextUrl(random, ts);
+                String service = nextService(random, app, (int) (index % serviceNumPerApp));
+
+                return new Object[]{
+                        ts,
+                        idc,
+                        host,
+                        0,
+                        service,
+                        url,
+                        random.nextDouble(0, 100), // cpu_util
+                        random.nextDouble(0, 100), // memory_util
+                        random.nextDouble(0, 100), // disk_util
+                        random.nextDouble(0, 100), // load_util
+                };
+            }
+        };
+    }
+
+    @Override
+    public long rowCount() {
+        return this.rowCount;
+    }
+
+    /**
+     * Returns a random idc, there are 20 idcs.
+     */
+    private String nextIdc(ThreadLocalRandom random) {
+        return "idc_" + random.nextInt(20);
+    }
+
+    /**
+     * Returns a random host name. Each IDC contains approximately 500 hosts, with a total of 10,000 hosts across all IDCs.
+     */
+    private String nextHost(ThreadLocalRandom random, String idc) {
+        return idc + "_host_" + random.nextInt(500);
+    }
+
+    /**
+     * Returns a random app name. There are 500 apps across all IDCs.
+     */
+    private String nextApp(ThreadLocalRandom random, String host) {
+        int hash = host.hashCode();
+        return "app_" + (hash % 500);
+    }
+
+    /**
+     * Returns a random service name. Each app contains 20 services.
+     */
+    private String nextService(ThreadLocalRandom random, String app, int index) {
+        return app + "_service_" + index;
+    }
+
+    /**
+     * Returns a random URL with a timestamp-based path.
+     * The URL format is: http://127.0.0.1/helloworld/{minutes}/{random_id}
+     * where random_id is between 0 and 1999.
+     */
+    private String nextUrl(ThreadLocalRandom random, long ts) {
+        long minutes = TimeUnit.MILLISECONDS.toMinutes(ts);
+        return String.format("http://127.0.0.1/helloworld/%d/%d", minutes, random.nextInt(2000));
+    }
+}
---
+++ src/main/java/io/greptime/bench/benchmark/BulkMetricsBenchmark.java
@@ -0,0 +1,108 @@
+package io.greptime.bench.benchmark;
+
+import io.greptime.BulkStreamWriter;
+import io.greptime.BulkWrite;
+import io.greptime.GreptimeDB;
+import io.greptime.bench.BulkMetricsTableDataProvider;
+import io.greptime.bench.DBConnector;
+import io.greptime.bench.TableDataProvider;
+import io.greptime.common.util.MetricsUtil;
+import io.greptime.common.util.SystemPropertyUtil;
+import io.greptime.metrics.ExporterOptions;
+import io.greptime.metrics.MetricsExporter;
+import io.greptime.models.Table;
+import io.greptime.models.TableSchema;
+import io.greptime.rpc.Compression;
+import io.greptime.rpc.Context;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Iterator;
+import java.util.concurrent.CompletableFuture;
+
+public class BulkMetricsBenchmark {
+
+    private static final Logger LOG = LoggerFactory.getLogger(BulkMetricsBenchmark.class);
+
+    public static void main(String[] args) throws Exception {
+        boolean zstdCompression = SystemPropertyUtil.getBool("zstd_compression", true);
+        int batchSize = SystemPropertyUtil.getInt("batch_size_per_request", 1000);
+        int maxRequestsInFlight = SystemPropertyUtil.getInt("max_requests_in_flight", 4);
+
+        LOG.info("Using zstd compression: {}", zstdCompression);
+        LOG.info("Batch size: {}", batchSize);
+        LOG.info("Max requests in flight: {}", maxRequestsInFlight);
+
+        Compression compression = zstdCompression ? Compression.Zstd : Compression.None;
+        Context ctx = Context.newDefault().withCompression(compression);
+
+        // Start a metrics exporter
+        MetricsExporter metricsExporter = new MetricsExporter(MetricsUtil.metricRegistry());
+        metricsExporter.init(ExporterOptions.newDefault());
+
+        GreptimeDB greptimeDB = DBConnector.connect();
+        BulkWrite.Config cfg = BulkWrite.Config.newBuilder()
+                .allocatorInitReservation(0)
+                .allocatorMaxAllocation(4 * 1024 * 1024 * 1024L)
+                .timeoutMsPerMessage(60000)
+                .maxRequestsInFlight(maxRequestsInFlight)
+                .build();
+
+        TableDataProvider tableDataProvider = new BulkMetricsTableDataProvider();
+        LOG.info("Table data provider: {}", tableDataProvider.getClass().getName());
+        tableDataProvider.init();
+        TableSchema tableSchema = tableDataProvider.tableSchema();
+
+        int shard = 0;
+
+        LOG.info("Start writing data");
+        try (BulkStreamWriter writer = greptimeDB.bulkStreamWriter(tableSchema, cfg, ctx)) {
+            Iterator<Object[]> rows = tableDataProvider.rows();
+
+            long start = System.nanoTime();
+            for (; ; ) {
+                Table.TableBufferRoot table = writer.tableBufferRoot(10240);
+                for (int i = 0; i < batchSize; i++) {
+                    if (!rows.hasNext()) {
+                        break;
+                    }
+
+                    Object[] row = rows.next();
+                    row[3] = shard % 2;
+
+                    table.addRow(row);
+                }
+                LOG.info("Table bytes used: {}", table.bytesUsed());
+                // Complete the table; adding rows is no longer permitted.
+                table.complete();
+
+                // Write the table data to the server
+                CompletableFuture<Integer> future = writer.writeNext();
+                long fStart = System.nanoTime();
+                future.whenComplete((r, t) -> {
+                    long costMs = (System.nanoTime() - fStart) / 1000000;
+                    if (t != null) {
+                        LOG.error("Error writing data, time cost: {}ms", costMs, t);
+                    } else {
+                        LOG.info("Wrote rows: {}, time cost: {}ms", r, costMs);
+                    }
+                });
+
+                shard++;
+
+                if (!rows.hasNext()) {
+                    break;
+                }
+            }
+
+            writer.completed();
+
+            LOG.info("Completed writing data, time cost: {}s", (System.nanoTime() - start) / 1000000000);
+        } finally {
+            tableDataProvider.close();
+        }
+
+        greptimeDB.shutdownGracefully();
+        metricsExporter.shutdownGracefully();
+    }
+}
